af8802b086b11c70f7ba3cca9e85d193443d4d0b
Vic1696 <victormwenda39@gmail.com>
2024-06-25 06:17:33 +0300

    first commit

---
+++ server/controllers/contentController.js
@@ -0,0 +1,50 @@
+const Content = require('../models/content');
+
+exports.createContent = async(req, res) =>{
+    const {title, body} =req.body;
+
+    try{
+        const newContent = new Content ({ title, body});
+        await newContent.save();
+        res.status(201).json(newContent);
+    }
+    catch(err){
+        res.status(400).json({error: err.message});
+    }
+};
+
+exports.getContent = async (req, res) => {
+    try{
+        const contents = await Content.find();
+        res.status(200).json(contents);
+    }
+    catch(err) {
+        res. status(400).json({error: err.message});
+    }
+};
+
+exports.updateContent = async (req, res) =>{
+    const {id} = req.params;
+    const {title, body} = req.body;
+
+    try{
+        const updatedContent = await Content.findByIdAndUpdate(id, {title, body}, {new:true});
+        res.status(200).json(updatedContent);
+    }
+    catch(err){
+        res.status(400).json({error: err.message});
+    }
+};
+
+exports.updateContent = async (req, res) =>{
+    const {id} = req.params;
+
+    try{
+        await Content.findByIdAndDelete(id);
+        res.status(200).json({message: 'Content deleted'});
+    }
+    catch(err){
+        res.status(400).json({error: err.message});
+    }
+};
+
